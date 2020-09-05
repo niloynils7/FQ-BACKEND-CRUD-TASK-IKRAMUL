f7743be6fe423288e3ad2ee0a7e3bb8762922538
Al Ikramul Hasan Niloy <34935153+niloynils7@users.noreply.github.com>
2020-09-06 02:42:24 +0600

    user model is implemented along with password hashing and an instance method to compare passwords

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 // Body parser, reading data from body into req.body
 app.use(express.json());
 
-//returns api request time
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
 // Routes
 app.use('/api/todolist', listRouter);
 
---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'user name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'password is required'],
+    minlength: 4,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'please confirm your password'],
+    validate: {
+      validator: function (el) {
+        return (el = this.password);
+      },
+      message: 'passwords are not same',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
