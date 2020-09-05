f1ec6434ef2820a92eae4c451ebfc40c381a2425
Al Ikramul Hasan Niloy <34935153+niloynils7@users.noreply.github.com>
2020-09-05 17:54:49 +0600

    successful connection of database. using mongo atlas.

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+/*// Development logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 // Body parser, reading data from body into req.body
 app.use(express.json());
+
+//returns api request time
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});*/
+
+// Routes
+//app.use('api/todolist');
+
+module.exports = app;
